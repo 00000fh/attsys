29b7f6ad7637a2940e283b52fac1488f37851123
fh <faizalhussin423@gmail.com>
2026-01-17 18:57:07 +0800

    Prepare project for production deployment

--- attsys/apps.py
+++ attsys/apps.py
@@ -1,6 +1,27 @@
+# attsys/apps.py
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
+import os
 
 class AttsysConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'attsys'
+
+    def ready(self):
+        User = get_user_model()
+
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+        if not username or not password:
+            return
+
+        if not User.objects.filter(is_superuser=True).exists():
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password,
+                role='ADMIN'
+            )
+            print('✅ Superuser created automatically')
